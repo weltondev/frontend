6ab016dc47a5333f107871ddda107686655a1a41
weltonDev <welton.developer95@gmail.com>
2024-03-07 19:15:38 -0300

    update buttons

--- js/relatorio.js
+++ js/relatorio.js
@@ -27,10 +27,10 @@
       <td>${dataCorreta}</td>
       <td>${entrada.observacao}</td>
       <td>
-          <a href="baixar.html?id=${entrada._id}&matricula=${entrada.matricula}&observacao=${entrada.observacao}&data=${entrada.data}" class="btn btn-primary"><i class="bi bi-file-earmark-arrow-down"></i></a>
-          <a href="editar.html?id=${entrada._id}&matricula=${entrada.matricula}" class="btn btn-secondary"><i class="bi bi-pencil"></i></a>
-          <a href="apagar.html?matricula=${entrada.matricula}&id=${entrada._id}&data=${entrada.data}" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-          <a href="info.html?id=${entrada.matricula}" class="btn btn-outline-secondary"><i class="bi bi-three-dots-vertical"></i></a>
+          <a href="baixar.html?id=${entrada._id}&matricula=${entrada.matricula}&observacao=${entrada.observacao}&data=${entrada.data}" class="btn btn-primary mb-1"><i class="bi bi-file-earmark-arrow-down"></i></a>
+          <a href="editar.html?id=${entrada._id}&matricula=${entrada.matricula}" class="btn btn-secondary mb-1"><i class="bi bi-pencil"></i></a>
+          <a href="apagar.html?matricula=${entrada.matricula}&id=${entrada._id}&data=${entrada.data}" class="btn btn-danger mb-1"><i class="bi bi-trash"></i></a>
+          <a href="info.html?id=${entrada.matricula}" class="btn btn-outline-secondary mb-1"><i class="bi bi-three-dots-vertical"></i></a>
       </td>
       `
       tabela.appendChild(tr)
